94871a4a1c84e04a7b33019759175110cb88a874
Programming Hub <Scottwillard9698@gmail.com>
2023-01-04 14:25:06 -0500

    Update Web Page Scraper (paragraph tags).py

--- Web Page Scraper (paragraph tags).py
+++ Web Page Scraper (paragraph tags).py
@@ -1,13 +1,12 @@
-import textwrap
-
 import requests
 from bs4 import BeautifulSoup
+import textwrap
 
-
-sitetoscrape = input("Enter a webpage to scrape: ")
+# Specify the webpage to scrape
+site_to_scrape = input("Enter a webpage to scrape: ")
 
 # send a request to the webpage
-response = requests.get(sitetoscrape)
+response = requests.get(site_to_scrape)
 
 # parse the HTML content of the webpage
 soup = BeautifulSoup(response.text, "html.parser")
@@ -15,6 +14,6 @@ soup = BeautifulSoup(response.text, "html.parser")
 # find all the <p> tags on the webpage
 paragraphs = soup.find_all("p")
 
-# print the content of each <p> tag to the console
+# print the content of each <p> tag to stdout
 for p in paragraphs:
     print(textwrap.fill(p.text, width=160))
